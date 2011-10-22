a44b3d346f3bc429a7c316f2073c22c00a91aec0
Ben Janik <ben@banjanik.com>
2011-10-22 18:03:20 -0400

    Facebook JS stuff possibly done.

--- js/script.js
+++ js/script.js
@@ -74,3 +74,30 @@ function getPage() {
         getPage.isLoaded = true;
     
 }
+// fb login
+window.fbAsyncInit = function() {
+  FB.init({
+    appId      : '268724529838969',
+    status     : true, 
+    cookie     : true,
+    xfbml      : true
+  });
+};
+// fb get js
+(function(d){
+   var js, id = 'facebook-jssdk'; if (d.getElementById(id)) {return;}
+   js = d.createElement('script'); js.id = id; js.async = true;
+   js.src = "//connect.facebook.net/en_US/all.js";
+   d.getElementsByTagName('head')[0].appendChild(js);
+ }(document));
+// fb post
+function updateStatusViaJavascriptAPICalling(){
+    var status  =   "Hello, World!";
+    FB.api('/me/feed', 'post', { message: status }, function(response) {
+        if (!response || response.error) {
+            alert('Error occured');
+        } else {
+            alert('Status updated Successfully');
+        }
+    });
+};
