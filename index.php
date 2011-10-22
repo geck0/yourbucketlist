c50cea218fdcc713a6c883b316bb1dee3df61e42
Nathan Cahill <natescahill@gmail.com>
2011-10-22 14:04:31 -0400

    Changing index fill to php

---
+++ index.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Before6 - Kick your online bucket list</title>
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <!-- Le HTML5 shim, for IE6-8 support of HTML elements -->
+    <!--[if lt IE 9]>
+      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+
+    <!-- Le styles -->
+    <link href="css/bootstrap.css" rel="stylesheet">
+    <link href="css/styles.css" rel="stylesheet">
+
+    <!-- Le fav and touch icons 
+    <link rel="shortcut icon" href="images/favicon.ico">
+    <link rel="apple-touch-icon" href="images/apple-touch-icon.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="images/apple-touch-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="images/apple-touch-icon-114x114.png"> -->
+  </head>
+
+  <body>
+
+    <nav role="navigation" class="topbar">
+      <div class="fill">
+        <div class="container">
+          <a class="brand" href="#">Before6</a>
+          <ul class="nav">
+            <li><a href="#home.html" rel="ajax">Home</a></li>
+            <li><a href="#create.html" rel="ajax">Create List</a></li>
+            <li><a href="#mylist.html" rel="ajax">My List</a></li>
+          </ul>
+        </div>
+      </div>
+    </nav> <!-- /navigation -->
+
+    <section class="container">
+	
+		<div id="loading">Loading...</div>
+		<article id="content"></article>
+	
+    </section> <!-- /container -->
+    
+    <footer class="container">
+      <p>&copy; Before6 2011</p>
+    </footer> <!-- /footer -->
+    
+	<section id="scripts">
+		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js"></script>
+		<script>window.jQuery||document.write('<script src="js/jquery-1.6.4.min.js"><\/script>');</script>
+		    
+		<script defer src="js/plugins.js"></script>
+		<script defer src="js/script.js"></script>
+		
+		<!-- Change UA-XXXXX-X to be your site's ID 
+		<script>
+		  window._gaq = [['_setAccount','UA-23292973-1'],['_trackPageview'],['_trackPageLoadTime']];
+		  Modernizr.load({
+		    load: ('https:' == location.protocol ? '//ssl' : '//www') + '.google-analytics.com/ga.js'
+		  });
+		</script> -->
+		
+		<!--[if lt IE 7 ]><script defer src="//ajax.googleapis.com/ajax/libs/chrome-frame/1.0.3/CFInstall.min.js"></script>
+		    <script defer>window.attachEvent("onload",function(){CFInstall.check({mode:"overlay"})});</script><![endif]-->
+		
+	</section>
+  </body>
+</html>
