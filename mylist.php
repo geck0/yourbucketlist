84720c5047621f1ab338abc750915dcc2fa42ac7
matt sornson <mattsornson@gmail.com>
2011-11-06 15:26:10 -0500

    accordion structure

--- mylist.php
+++ mylist.php
@@ -20,13 +20,21 @@
    
    <p class="heading">Result-2</p>
    
-   <div class="content">Lorem ipsum dolor sit amet, consectetuer adipiscing elit orem ipsum dolor sit amet, consectetuer adipiscing elit</div>
+   <div class="content">
+   		<div class="option1">Click here for option 1</div>
+   		<div class="option2">Click here for option 2</div>
+   		<div class="option3">Click here for option 3</div>
+   		</div>
    
    <p class="heading">Result-3</p>
    
-   <div class="content">Lorem ipsum dolor sit amet, consectetuer adipiscing elit orem ipsum dolor sit amet, consectetuer adipiscing elit</div>
+   <div class="content">
+   		<div class="option1">Click here for option 1</div>
+   		<div class="option2">Click here for option 2</div>
+   		<div class="option3">Click here for option 3</div>
+   		</div>
    
-   </div>
+  </div>
       	   
       	   
       	
