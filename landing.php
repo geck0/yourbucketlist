e8e300c0d2bb4087259e6c320536ba82a209b1e2
Nathan Cahill <natescahill@gmail.com>
2011-11-06 21:59:14 -0500

    Login link

--- landing.php
+++ landing.php
@@ -6,8 +6,8 @@
 	  
 	  <p><h2>Create a list of your life goals. Organise your list and plan it out.<br> Share your list with your friends. Accomplish your goals. Live your dreams. <br>Login to get started!</h2></p>
 	  <div id="right">
-	      <div id="loginbtn" onClick="FB.login(login, {'scope':'publish_actions,offline_access,email,user_about_me,user_birthday'})"></div>
-	           </div>
+	      <a href="<?php echo $loginUrl; ?>"><div id="loginbtn"></div></a>
+	  </div>
 	     
 	</div>
 </section> <!-- /container -->
