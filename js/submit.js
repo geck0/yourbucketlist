5c2c2a03f74f4adca9a61ff75289f601cdd2f196
Ben Janik <ben@banjanik.com>
2011-11-10 22:30:02 -0500

    Wrote JS for creating and submitting lists of any length, created test file

---
+++ js/submit.js
@@ -0,0 +1,63 @@
+(function ($) {
+
+	$(document).ready(function () {
+	
+		var list = [],
+			postString = '',
+			email = getCookie("email"),
+			name = getCookie("name");
+		
+		submitHandler();
+		
+		function submitHandler () {
+			$('#addItemButton').click(function () {
+			
+				addToList();
+				createAndPostString();
+				
+			});
+		}
+		
+		function addToList () {
+			var listItem = '';
+		
+			listItem = $('#listInput').val();
+			$('<div><h2>' + listItem + '</h2></div>').insertBefore($('#bucketlist > div:first');
+		
+		}
+		
+		function createAndPostString () {
+		
+			list = $('#bucketList > div > h2').map(function() { return this.innerHTML; });
+			
+			for (var i = 0, j = list.length; i < j; i++) {
+				postString += '&input' + i + '=' + list[i];	
+			}
+			
+			postString.slice(1);
+			postString += '&email=' + email + '&name=' + name;
+			
+			$.ajax({  
+				type: "POST",  
+				url: "createlist.php",  
+				data: postString,  
+				success: function() {  
+					
+				}  
+			});
+		
+		}
+		
+		function getCookie ( cookieName ) {
+			var results = document.cookie.match ( '(^|;) ?' + cookieName + '=([^;]*)(;|$)' );
+			
+			if ( results ) {
+				return ( unescape ( results[2] ) );
+			} else {
+				return null;
+			}
+		}
+	
+	});
+
+})(jQuery);
---
+++ test.php
@@ -0,0 +1,4 @@
+<?php require('header.php'); ?>
+
+
+<?php require('footer.php'); ?>
